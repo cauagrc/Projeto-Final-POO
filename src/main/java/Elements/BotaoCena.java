7329c04a4c9ca772c413f92b21411986797a51d0
Samuel-Amadio <amadiosamuel06@gmail.com>
2026-06-08 17:57:47 -0300

    Update BotaoCena.java

--- src/main/java/Elements/BotaoCena.java
+++ src/main/java/Elements/BotaoCena.java
@@ -5,24 +5,24 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class BotaoCena extends Button{
-    private Stage janelaPrincipal;
 
     public BotaoCena() {
         super();
     }
     
-    public BotaoCena(Stage janelaPrincipal) {
-    	super();
-        this.janelaPrincipal = janelaPrincipal;
-    }
-
-    
     public void mudarPara(Scene novaCena) {
-        if (novaCena != null && janelaPrincipal != null) {
-            janelaPrincipal.setScene(novaCena);
-            janelaPrincipal.show();
+        if (novaCena != null) {
+            
+        	Stage janelaPrincipal = (Stage) this.getScene().getWindow();
+        	
+        	if(janelaPrincipal != null) {
+        		janelaPrincipal.setScene(novaCena);
+        		janelaPrincipal.show();
+        	}else {
+        		System.err.println("Erro: Não foi possível encontrar a janela atual");
+        	}
         } else {
-            System.err.println("Erro: Janela ou Nova Cena estão nulas!");
+            System.err.println("Erro: Nova Cena está nula!");
         }
     }
 }
