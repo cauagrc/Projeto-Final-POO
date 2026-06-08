0e0e03b578933a29b41d21aac0ea36b73834c809
marucs-vp <marcusvnp2112@gmail.com>
2026-06-08 10:48:47 -0400

    Criado o menu (sem parte estetica) dos grupos
    tive que retirar a verificacao de erro pq estava causando erro
    Alterei o funcionamento do botao para os eventos acontecerem dentro do Controller

--- src/main/java/game/poo/controllers/MenuController.java
+++ src/main/java/game/poo/controllers/MenuController.java
@@ -3,22 +3,42 @@ package game.poo.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Elements.BotaoGrupo;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class MenuController implements Initializable {
 	@FXML
-	private Label label;
+	private Label descricao;
 
+	@FXML
+	private VBox painelGrupos;
+	
+	@FXML
+	private BotaoGrupo grupo1;
+
+	@FXML
+	private BotaoGrupo grupo2;
+
+	@FXML
+	private BotaoGrupo grupo3;
+	
+	@FXML
+	private void enviarInfos(ActionEvent event) {
+
+	    BotaoGrupo selecionado = (BotaoGrupo) event.getSource();
+
+	    descricao.setText(selecionado.getDescricao());
+	    
+	    
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
-		String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        
-        label.setText("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 	}
 
 }
